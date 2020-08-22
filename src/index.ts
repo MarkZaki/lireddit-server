4d321d866811b52b9de5d165a2e2f7cf0609ac2d
Mark Zaky <markzaky18@gmail.com>
2020-08-22 17:44:13 +0200

    setup apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
-import { config as dotenv } from "dotenv";
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { config as dotenv } from "dotenv";
 import mikroConfig from "./mikro-orm.config";
+import { PostResolver } from "./resolvers/post";
 
 dotenv();
 
 const main = async () => {
 	const orm = await MikroORM.init(mikroConfig);
 	await orm.getMigrator().up();
+
+	const app = express();
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [PostResolver],
+			validate: false
+		})
+	});
+
+	apolloServer.applyMiddleware({ app });
+
+	app.listen(process.env.PORT, () => console.log("Server Running..."));
 };
 
 main().catch(err => console.log(err));
